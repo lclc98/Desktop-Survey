ce55dfc74ffc815019005f1557c291f76ca423bb
lclc98 <liam.c14785@gmail.com>
2019-01-22 20:00:36 +1000

    Fixed Complete page

--- src/renderer/components/Complete.vue
+++ src/renderer/components/Complete.vue
@@ -1,9 +1,13 @@
 <template>
-  <div>
-    Complete
-    <a class="button is-large is-fullwidth" v-on:click="next">
-      Done
-    </a>
+  <div class="columns">
+    <div class="column"></div>
+    <div class="column is-two-thirds">
+      <h1 class="title has-text-centered">Complete</h1>
+      <a class="button is-large is-fullwidth" v-on:click="next">
+        Done
+      </a>
+    </div>
+    <div class="column"></div>
   </div>
 </template>
 
